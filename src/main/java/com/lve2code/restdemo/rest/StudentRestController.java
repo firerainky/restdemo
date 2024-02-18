4c0a65f7955c96e2274ecf7e3c57462c3dc6e589
zky <zhengkanyan@gmail.com>
2024-02-19 07:15:37 +0800

    Added endpoint for getting a single student

--- src/main/java/com/lve2code/restdemo/rest/StudentRestController.java
+++ src/main/java/com/lve2code/restdemo/rest/StudentRestController.java
@@ -1,6 +1,8 @@
 package com.lve2code.restdemo.rest;
 
 import com.lve2code.restdemo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +12,27 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    // define endpoint for "/students" - return a list students
-    @RequestMapping("/students")
-    public List<Student> sayHello() {
-        List<Student> students = new ArrayList<>();
+
+    private List<Student> students;
+
+    // define the @PostConstruct to load the student data ... only once!
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
         students.add(new Student("Poornima", "Patel"));
         students.add(new Student("Mario", "Rossi"));
         students.add(new Student("Mary", "Smith"));
+    }
+
+    // define endpoint for "/students" - return a list students
+    @RequestMapping("/students")
+    public List<Student> sayHello() {
         return students;
+    }
+
+    // define endpoint for "/students/{studentId}" - return student at index
+    @RequestMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return students.get(studentId);
     }
 }
